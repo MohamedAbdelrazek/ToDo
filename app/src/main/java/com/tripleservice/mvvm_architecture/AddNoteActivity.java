340562ebb9540b8e287a33bc2424675e0242d9d0
MohamedAbdelrazek <muhamedrabi3@gmail.com>
2019-10-03 04:01:27 +0200

    Implementing  Adding new notes .....

---
+++ app/src/main/java/com/tripleservice/mvvm_architecture/AddNoteActivity.java
@@ -0,0 +1,87 @@
+package com.tripleservice.mvvm_architecture;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    public static String EXTRA_TITLE = "com.tripleservice.mvvm_architecture.EXTRA_TITLE";
+    public static String EXTRA_DESC = "com.tripleservice.mvvm_architecture.EXTRA_DESC";
+    public static String EXTRA_PRIORITY = "com.tripleservice.mvvm_architecture.EXTRA_PRIORITY";
+
+
+    @BindView(R.id.activity_add_note_title_edit_text)
+    EditText activityAddNoteTitleEditText;
+    @BindView(R.id.activity_add_note_desc_edit_text)
+    EditText activityAddNoteDescEditText;
+    @BindView(R.id.activity_add_note_priority_number_picker)
+    NumberPicker activityAddNotePriorityNumberPicker;
+    @BindView(R.id.activity_add_note_priority_edit_text)
+    TextView activityAddNotePriorityEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        ButterKnife.bind(this);
+        activityAddNotePriorityNumberPicker.setMinValue(1);
+        activityAddNotePriorityNumberPicker.setMaxValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Note");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_note: {
+                saveNote();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+    private void saveNote() {
+        String title = activityAddNoteTitleEditText.getText().toString();
+        String desc = activityAddNoteDescEditText.getText().toString();
+        int priority = activityAddNotePriorityNumberPicker.getValue();
+
+        if (title.trim().isEmpty() | desc.trim().isEmpty()) {
+            Toast.makeText(this, "Please insert a title and description !", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_TITLE, title);
+        intent.putExtra(EXTRA_DESC, desc);
+        intent.putExtra(EXTRA_PRIORITY, priority);
+        setResult(RESULT_OK, intent);
+        finish();
+
+    }
+}
--- app/src/main/java/com/tripleservice/mvvm_architecture/MainActivity.java
+++ app/src/main/java/com/tripleservice/mvvm_architecture/MainActivity.java
@@ -1,13 +1,18 @@
 package com.tripleservice.mvvm_architecture;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.tripleservice.mvvm_architecture.adapter.NotesAdapter;
 import com.tripleservice.mvvm_architecture.adapter.OnItemClicked;
 
@@ -18,6 +23,10 @@ import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity implements OnItemClicked {
+
+    private static final int RESULT_CODE = 121;
+    @BindView(R.id.floatingActionButton)
+    FloatingActionButton addActionButton;
 
     private NoteViewModel mNoteViewModel;
     private NotesAdapter mNotesAdapter;
@@ -36,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements OnItemClicked {
         mNotesRecyclerView.setHasFixedSize(true);
         mNotesAdapter = new NotesAdapter(this);
         mNotesRecyclerView.setAdapter(mNotesAdapter);
+
         mNoteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         mNoteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
@@ -45,11 +55,33 @@ public class MainActivity extends AppCompatActivity implements OnItemClicked {
                 mNotesAdapter.updateNotes(notes);
             }
         });
+        addActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity.this, AddNoteActivity.class), RESULT_CODE);
+            }
+        });
     }
 
     @Override
     public void onClick(Note note) {
         //handle recycler click events...
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_CODE && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String desc = data.getStringExtra(AddNoteActivity.EXTRA_DESC);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY, 1);
+            mNoteViewModel.insert(new Note(title, desc, priority));
+            Toast.makeText(this, "Note Saved !! ", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Note not Saved !! ", Toast.LENGTH_SHORT).show();
+        }
     }
 }
